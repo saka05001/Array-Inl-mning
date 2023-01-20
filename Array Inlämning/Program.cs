393a958236a054372ef5ceef564fa10db75bfae5
Pingvu <nhlpool27@gmail.com>
2023-01-20 13:49:35 +0100

    Add project files.

---
+++ Array Inlämning/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv hur många namn du ska skriva");
+            int antal = int.Parse(Console.ReadLine());
+            string[] namn = new string[antal];
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("Skriv ett namn");
+                namn[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Här är dem namnen du skrev in");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
